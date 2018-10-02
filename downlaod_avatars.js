c3dad58ba73d2e9f7d58732b51ae9237e2084d33
Gaurav <netgary7@gmail.com>
2018-10-02 03:49:24 +0000

    Finished step 7

--- downlaod_avatars.js
+++ downlaod_avatars.js
@@ -1,3 +1,32 @@
 var request = require('request');
+var secret = require('./secrets.js');
+
 console.log('Welcome to the Github Avatar Downloader!');
 
+function getRepoContributors(repoOwner, repoName, handleAvatarURL){
+    var options = {
+        url:  "https://api.github.com/repos/" + repoOwner + "/" + repoName + "/contributors",
+        headers: {
+        'User-Agent':'request',
+        'Authorization': 'token ' + secret.GITHUB_TOKEN
+       }
+    };
+
+    request(options, function(err, res, body) {
+    // handleAvatarURL(err, body);
+    var parsedData = JSON.parse(body)
+    for (var i = 0; i< parsedData.length ; i++){
+        var contributor = parsedData[i];
+        handleAvatarURL(contributor.avatar_url);
+    }
+  });
+}
+
+var fetchAvatarURL = function(aURL){
+    console.log(aURL);
+}
+
+
+
+
+getRepoContributors("jquery", "jquery", fetchAvatarURL);
