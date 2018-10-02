859785c9270dca0502af5fb4e15fe5631a438e10
Gaurav <netgary7@gmail.com>
2018-10-02 15:38:49 +0000

    Step * added

--- downlaod_avatars.js
+++ downlaod_avatars.js
@@ -1,5 +1,6 @@
 var request = require('request');
 var secret = require('./secrets.js');
+var fs = require('fs');
 
 console.log('Welcome to the Github Avatar Downloader!');
 
@@ -17,21 +18,25 @@ function getRepoContributors(repoOwner, repoName, handleAvatarURL){
     var parsedData = JSON.parse(body)
     for (var i = 0; i< parsedData.length ; i++){
         var contributor = parsedData[i];
-        handleAvatarURL(contributor.avatar_url);
+        handleAvatarURL(contributor.avatar_url, contributor.login);
     }
   });
 }
 
-var fetchAvatarURL = function(avatar_url, filepath){
-    var options = {
-        url:
-    }
-request.get(avatar_url)
-       .on('error', function (err) {
-         throw err;
+// var fetchAvatarURL = function(avatar_url, filepath){
+
+//  request.get(avatar_url)
+//        .pipe(fs.createWriteStream(filepath))
+
+ // }
+
+ function downloadImageByURL(avatarURL, userName) {
+
+  request(avatarURL).pipe(fs.createWriteStream('./avatars/' + userName +'.jpg'));         // Note 1
+
 }
 
+// fetchAvatarURL("https://avatars2.githubusercontent.com/u/2741?v=3&s=466", "avatars/kvirani.jpg")
 
 
-
-getRepoContributors("jquery", "jquery", fetchAvatarURL);
+getRepoContributors("jquery", "jquery", downloadImageByURL);
